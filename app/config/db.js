24a8bafed836a89b202c2d77f7d855d58bdd8471
leadstar116 <leadstar@LeadStars-iMac.local>
2019-05-31 09:38:16 +1000

    db config

--- app/config/db.js
+++ app/config/db.js
@@ -1,7 +1,8 @@
 'user strict';
 
-var mysql = require('mysql');
-//local mysql db connection
+var mysql = require('./node_modules/mysql');
+//heroku
+
 var connection = mysql.createConnection({
     host     : 'us-cdbr-iron-east-02.cleardb.net',
     user     : 'b3e7cb0a79dc04',
@@ -9,8 +10,17 @@ var connection = mysql.createConnection({
     database : 'heroku_5b9f51c996f2516',
 });
 
+//local
+/*
+var connection = mysql.createConnection({
+    host     : 'us-cdbr-iron-east-02.cleardb.net',
+    user     : 'b3e7cb0a79dc04',
+    password : '1514a61a',
+    database : 'heroku_5b9f51c996f2516',
+});
+*/
 connection.connect(function(err) {
     if (err) throw err;
-});
+});sas
 
 module.exports = connection;
