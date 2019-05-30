795d5569227d0d09c89b493c72ae5085f0feea7f
leadstar116 <leadstar@LeadStars-iMac.local>
2019-05-31 08:43:47 +1000

    db fix

--- server.js
+++ server.js
@@ -3,19 +3,6 @@ const express = require('express'),
     bodyParser = require('body-parser');
 port = process.env.PORT || 3000;
 
-
-const mysql = require('mysql');
-// connection configurations
-const mc = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'mydb'
-});
-
-// connect to database
-mc.connect();
-
 app.listen(port);
 
 console.log('API server started on: ' + port);
