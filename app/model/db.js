5805dda24cd457c61c7ff521105ad7727136faac
leadstar116 <leadstar@LeadStars-iMac.local>
2019-05-31 09:24:29 +1000

    db for heroku

--- app/model/db.js
+++ app/model/db.js
@@ -1,13 +1,12 @@
 'user strict';
 
 var mysql = require('mysql');
-aasadf
 //local mysql db connection
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'root',
-    database : 'PostApp',
+    host     : 'us-cdbr-iron-east-02.cleardb.net',
+    user     : 'b3e7cb0a79dc04',
+    password : '1514a61a',
+    database : 'heroku_5b9f51c996f2516',
 });
 
 connection.connect(function(err) {
